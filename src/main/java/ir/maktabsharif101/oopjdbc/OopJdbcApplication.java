19cdb686e42c3447cdc6fd75b89f28aa6bbde58e
maxixking66 <maxixking66@gamil.com>
2023-09-21 14:28:07 +0330

    refactor ApplicationContext.java

--- src/main/java/ir/maktabsharif101/oopjdbc/OopJdbcApplication.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/OopJdbcApplication.java
@@ -2,18 +2,17 @@ package ir.maktabsharif101.oopjdbc;
 
 import ir.maktabsharif101.oopjdbc.service.PermissionService;
 import ir.maktabsharif101.oopjdbc.service.RoleService;
-import ir.maktabsharif101.oopjdbc.service.UserService;
 import ir.maktabsharif101.oopjdbc.util.ApplicationContext;
 
 public class OopJdbcApplication {
 
     public static void main(String[] args) {
 
-        RoleService roleService = ApplicationContext.getRoleService();
+        ApplicationContext applicationContext = ApplicationContext.getInstance();
 
-        UserService userService = ApplicationContext.getUserService();
+        PermissionService permissionService = applicationContext.getPermissionService();
 
-        PermissionService permissionService = ApplicationContext.getPermissionService();
+        RoleService roleService = applicationContext.getRoleService();
 
 //        impl logic
 
