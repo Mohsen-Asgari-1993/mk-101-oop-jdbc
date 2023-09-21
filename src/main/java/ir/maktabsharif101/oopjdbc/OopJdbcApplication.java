5af1bff01cad03f0d9e37034a903ebaf11376cd2
maxixking66 <maxixking66@gamil.com>
2023-09-21 14:15:14 +0330

    add ApplicationContext

--- src/main/java/ir/maktabsharif101/oopjdbc/OopJdbcApplication.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/OopJdbcApplication.java
@@ -1,8 +1,21 @@
 package ir.maktabsharif101.oopjdbc;
 
+import ir.maktabsharif101.oopjdbc.service.PermissionService;
+import ir.maktabsharif101.oopjdbc.service.RoleService;
+import ir.maktabsharif101.oopjdbc.service.UserService;
+import ir.maktabsharif101.oopjdbc.util.ApplicationContext;
+
 public class OopJdbcApplication {
 
     public static void main(String[] args) {
+
+        RoleService roleService = ApplicationContext.getRoleService();
+
+        UserService userService = ApplicationContext.getUserService();
+
+        PermissionService permissionService = ApplicationContext.getPermissionService();
+
+//        impl logic
 
     }
 }
